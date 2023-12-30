8d161c6486a4f2c53bbc364adfc0b0fd309b34fa
Gonzalo <gonalc8@gmail.com>
2023-12-30 12:48:42 +0100

    show images on screen

--- src/features/Pictures/index.tsx
+++ src/features/Pictures/index.tsx
@@ -1,30 +1,53 @@
-import { StyleSheet, Text, View } from "react-native"
-import useBucket from "../../utils/hooks/bucket"
-import Logout from "../Logout"
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import useBucket from "../../utils/hooks/bucket";
+import Logout from "../Logout";
 
 const Pictures = () => {
-    useBucket()
-    
-    return (
-        <View style={styles.container}>
-            <Text>Holaaa pictures</Text>
+  const files = useBucket();
 
-            <View style={styles.logoutContainer}>
-                <Logout />
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text>Holaaa pictures</Text>
+
+      <View style={styles.picturesContainer}>
+        <FlatList
+          data={files}
+          renderItem={({ item }) => {
+            const { name, url } = item;
+
+            return (
+              <View>
+                <Text>{name}</Text>
+                <Image
+                  source={{ uri: url }}
+                  style={{ height: 200, width: 350 }}
+                />
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+
+      <View style={styles.logoutContainer}>
+        <Logout />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20
-    },
-    logoutContainer: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  picturesContainer: {
+    paddingVertical: 10
+  },
+  logoutContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default Pictures
+export default Pictures;
--- src/utils/hooks/bucket.ts
+++ src/utils/hooks/bucket.ts
@@ -1,20 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../../services/supabase";
+import type { FileObject } from "@supabase/storage-js/src/lib/types";
 
 type BucketName = "pictures";
 
-const useBucket = (bucketName: BucketName = "pictures") => {
-  useEffect(() => {
-    supabase.storage.getBucket(bucketName).then(({ data }) => {
-      console.log("SINGLE BUCKET: ", data);
-    });
-  }, []);
-
-  useEffect(() => {
-    supabase.storage.listBuckets().then(({ data }) => {
-      console.log("ALL BUCKET: ", data);
-    });
-  }, []);
+type FileWithURL = FileObject & {
+  url: string;
 };
 
-export default useBucket
+const ONE_HOUR = 3600;
+
+const useBucket = (bucketName: BucketName = "pictures") => {
+  const [files, setFiles] = useState<FileWithURL[]>([]);
+
+  useEffect(() => {
+    supabase.storage
+      .from(bucketName)
+      .list()
+      .then(async ({ data }) => {
+        const parsedFiles: FileWithURL[] = [];
+
+        if (data) {
+          for (const file of data) {
+            const isEmpty = !file.metadata.size;
+
+            if (!isEmpty) {
+              const signedData = await supabase.storage
+                .from(bucketName)
+                .createSignedUrl(file.name, ONE_HOUR);
+
+              const parsedFile: FileWithURL = {
+                ...file,
+                url: signedData.data?.signedUrl || "",
+              };
+
+              parsedFiles.push(parsedFile);
+            }
+          }
+        }
+
+        setFiles(parsedFiles);
+      });
+  }, []);
+
+  return files;
+};
+
+export default useBucket;
