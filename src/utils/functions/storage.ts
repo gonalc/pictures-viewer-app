33107db1f3aa18f2254e6838d96bd4b7a3e7022b
Gonzalo <gonalc8@gmail.com>
2024-01-07 10:28:39 +0100

    sort pictures by date

--- src/utils/functions/storage.ts
+++ src/utils/functions/storage.ts
@@ -1,28 +1,36 @@
 import { supabase } from "../../services/supabase";
 import type { BucketName, FileWithURL } from "../hooks/bucket";
 
-export async function getImages(bucketName: BucketName = 'pictures') {
-    const { data, error } = await supabase.storage
-    .from(bucketName)
-    .list()
+export async function getImages(bucketName: BucketName = "pictures") {
+  const { data, error } = await supabase.storage.from(bucketName).list("", {
+    sortBy: {
+      column: "created_at",
+      order: "desc",
+    },
+  });
 
-    if (error) {
-        console.log('There has been an error fetching images: ', error)
-    }
+  if (error) {
+    console.log("There has been an error fetching images: ", error);
+  }
 
-    return data
+  return data;
 }
 
-export async function deleteImage(file: FileWithURL, bucketName: BucketName = 'pictures') {
-    const { data, error } = await supabase.storage.from(bucketName).remove([file.name])
+export async function deleteImage(
+  file: FileWithURL,
+  bucketName: BucketName = "pictures"
+) {
+  const { data, error } = await supabase.storage
+    .from(bucketName)
+    .remove([file.name]);
 
-    if (error) {
-        console.log('There has been an error removing this file: ', error)
-    }
+  if (error) {
+    console.log("There has been an error removing this file: ", error);
+  }
 
-    if (!data) {
-        return null
-    }
+  if (!data) {
+    return null;
+  }
 
-    return data[0].id
+  return data[0].id;
 }
